a9e3366fdcb68dd84c1cd33ee58a268523ce79b2
Chris <cellis8200@gmail.com>
2017-04-30 08:46:50 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,19 @@
+//set secret number
+var secretNumber = 4;
+
+//ask user for guess
+var guess = Number(prompt("Guess a number"));
+
+//check if guess is right
+
+if (guess === secretNumber){
+	alert("You got it right!");
+}
+//otherwise, check if higher
+else if(Number(guess) > secretNumber){
+	alert("Too high, guess again!");
+}
+//otherwise check if lower
+else{
+	alert("Too low, guess again!");
+}
